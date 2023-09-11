88bccda21ba4369292d2b69b8d8c054e13c95222
Constantino Sorto Reyes <cos.sorto@gmail.com>
2023-09-10 19:51:01 -0600

    add routes to auth middleware

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('samples', \App\Http\Controllers\SampleController::class);
     Route::get('get-municipalities', [\App\Http\Controllers\MunicipalityController::class, 'getMunicipalities'])->name('getMunicipalities');
+    Route::resource('sampleDetails', \App\Http\Controllers\SampleDetailController::class);
 });
-Route::resource('sampleDetails', \App\Http\Controllers\SampleDetailController::class);
 
